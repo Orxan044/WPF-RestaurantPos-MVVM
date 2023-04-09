03ed40d0213a5845cd24897fa11a7919957e3fe6
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2023-04-09 15:03:04 +0400

    Add project files.

---
+++ WPF - RestaurantPos - MVVM/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF___RestaurantPos___MVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Table> Tables { get; set; }
+        public ObservableCollection<Table> TempTables { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            Tables = new ObservableCollection<Table>
+            {
+                new Table {TableName = "Table 1" , status=true ,tapleType=Table.TapleType.Table },
+                new Table {TableName = "Table 2" , status=true , tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 3" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 4" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 5" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 6" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 7" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 8" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 9" , status = true, tapleType=Table.TapleType.Table},
+                new Table {TableName = "Table 10" , status = true, tapleType=Table.TapleType.Table}
+            };
+
+
+        }
+
+
+        private void CloseImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ WPF - RestaurantPos - MVVM/Table.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF___RestaurantPos___MVVM
+{
+    public class Table : INotifyPropertyChanged
+    {
+        public enum TapleType { Table, Capinet }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private string? tableName;
+
+        public bool status { get; set; }
+
+
+        public string TableName
+        {
+            get { return tableName; }
+            set
+            {
+                tableName = value;
+                OnPropertyRaised();
+            }
+        }
+
+        public bool Status
+        {
+            get { return status; }
+            set
+            {
+                status = value;
+                OnPropertyRaised();
+            }
+        }
+
+
+        public TapleType tapleType;
+
+
+
+        private void OnPropertyRaised([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+    }
+}
